bd4c88aea5fd39b0522c487e056b7ecd7b0121a7
Philip Mortiboy <philip.mortiboy@thestreet.com>
2017-04-24 12:56:02 +0100

    Initial commit

---
+++ SearchListViewExample/index.android.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class SearchListViewExample extends Component {
+  render() {
+    return (
+      <View style={{paddingTop: 10}}>
+        <Text>Android support coming soon</Text>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('SearchListViewExample', () => SearchListViewExample);
